0c43947821bfe714019969619f4066dc696de7ff
Denny Serejo <denny_serejo@hotmail.com>
2017-07-30 12:00:21 -0300

    First commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var ExpressPeerServer = require('peer').ExpressPeerServer;
-var Lobby = require('src/server/Lobby');
+var Lobby = require('./src/server/Lobby');
 var options = {
     allow_discovery:true
 }
-module.exports = function(http){
+module.exports = function(http,app){
     var peerjs = ExpressPeerServer(http, options)
     app.use('/peerjs', peerjs);
     return new Lobby(http,peerjs);
